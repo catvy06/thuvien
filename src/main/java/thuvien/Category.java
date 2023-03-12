9a0bdd4ee3c4af75250340c7df36841bf85ae634
catvy06 <95401506+catvy06@users.noreply.github.com>
2023-03-12 21:02:30 +0700

    Create Category.java
    category

---
+++ src/main/java/thuvien/Category.java
@@ -0,0 +1,46 @@
+package thuvien;
+
+public abstract class Category {
+    public int id;
+    public String name;
+    public boolean status;
+
+    public Category(int id, String name, boolean status){
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
